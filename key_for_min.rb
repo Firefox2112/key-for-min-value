d8fd03ff96ce083e153142e478938c110feac3f2
Coby <cobyrpilpel2@gmail.com>
2020-12-16 02:51:54 -0500

    commit tree 28f3cd585d5491054b80282060b6b971c3d66fa1

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+low_val = 0
+low_key = nil
+name_hash.each do |key, value|
+    if low_val == 0 || value < low_val
+        low_val = value
+        low_key = key
+    end
 end
+low_key
+end
+
+# Tried multiple methods. Restrictions added difficulty/confusion. didn't realize value/values different operations. more reading required.
